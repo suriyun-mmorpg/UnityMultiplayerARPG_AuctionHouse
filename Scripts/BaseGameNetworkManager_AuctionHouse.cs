f242132bed9d11bf5ca7c05ab59f55cf7b20cc7d
Insthync <ittipon.bay@gmail.com>
2021-10-25 18:09:41 +0700

    Add create auction skeleton function

---
+++ Scripts/BaseGameNetworkManager_AuctionHouse.cs
@@ -0,0 +1,27 @@
+﻿using LiteNetLibManager;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MultiplayerARPG
+{
+    public partial class BaseGameNetworkManager
+    {
+        /*
+         * Do `Item Listing`, `Bidding`, `Buying` directly with service
+         */
+        [Header("Auction House")]
+        public ushort createAuctionMsgType = 300;
+
+        [DevExtMethods("RegisterServerMessages")]
+        protected void RegisterServerMessages_AuctionHouse()
+        {
+            RegisterServerMessage(createAuctionMsgType, HandleCreateAuctionAtServer);
+        }
+
+        private void HandleCreateAuctionAtServer(MessageHandlerData messageHandler)
+        {
+
+        }
+    }
+}
