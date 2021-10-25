d27c317c54526ad8ac781bad355d80f8e5bbda05
Insthync <ittipon.bay@gmail.com>
2021-10-25 18:29:21 +0700

    WIP auction creating

--- Scripts/BaseGameNetworkManager_AuctionHouse.cs
+++ Scripts/BaseGameNetworkManager_AuctionHouse.cs
@@ -8,7 +8,7 @@ namespace MultiplayerARPG
     public partial class BaseGameNetworkManager
     {
         /*
-         * Do `Item Listing`, `Bidding`, `Buying` directly with service
+         * Do `Item Listing`, `Sell History`, `Bid Listing`, `Bidding`, `Buying` directly with service
          */
         [Header("Auction House")]
         public ushort createAuctionMsgType = 300;
@@ -21,7 +21,27 @@ namespace MultiplayerARPG
 
         private void HandleCreateAuctionAtServer(MessageHandlerData messageHandler)
         {
+            IPlayerCharacterData playerCharacterData;
+            if (!ServerUserHandlers.TryGetPlayerCharacter(messageHandler.ConnectionId, out playerCharacterData))
+            {
+                // Do nothing, player character is not enter the game yet.
+                return;
+            }
+            // Require index of non equip items, amount, starting auction price, buyout price (optional, 0 = no buyout)
+            // Check player's item, then tell the service to add to bidding list, and remove it from inventory
+            CreateAuctionMessage createAuction = messageHandler.ReadMessage<CreateAuctionMessage>();
+            if (createAuction.amount <= 0)
+                createAuction.amount = 1;
+            if (createAuction.indexOfItem >= playerCharacterData.NonEquipItems.Count ||
+                playerCharacterData.NonEquipItems[createAuction.indexOfItem].amount < createAuction.amount)
+            {
+                // Do nothing, wrong index of item or item amount is over than it has
+                return;
+            }
+            // Tell the service to add to bidding list
 
+            // Remove item from inventory
+            playerCharacterData.DecreaseItemsByIndex(createAuction.indexOfItem, createAuction.amount);
         }
     }
 }
---
+++ Scripts/CreateAuctionMessage.cs
@@ -0,0 +1,28 @@
+﻿using LiteNetLib.Utils;
+
+namespace MultiplayerARPG
+{
+    public struct CreateAuctionMessage : INetSerializable
+    {
+        public int indexOfItem;
+        public short amount;
+        public int startPrice;
+        public int buyoutPrice;
+
+        public void Deserialize(NetDataReader reader)
+        {
+            indexOfItem = reader.GetPackedInt();
+            amount = reader.GetPackedShort();
+            startPrice = reader.GetPackedShort();
+            buyoutPrice = reader.GetPackedShort();
+        }
+
+        public void Serialize(NetDataWriter writer)
+        {
+            writer.PutPackedInt(indexOfItem);
+            writer.PutPackedShort(amount);
+            writer.PutPackedInt(startPrice);
+            writer.PutPackedInt(buyoutPrice);
+        }
+    }
+}
